e04abad9212999db14c04f048e4358b7c38c35ee
Deman <deman2805@gmail.com>
2025-02-15 23:25:52 +0200

    change 5 generic task

--- src/generics/5.ts
+++ src/generics/5.ts
@@ -1,20 +1,13 @@
-// зіставити: роль - опис (ключ K - значення T(тип))
-// Record<K, T>
-//enum - перелік;
-
 export enum UserRole {
-  admin = "admin",
-  editor = "editor",
-  guest = "guest",
+  admin = 'admin',
+  editor = 'editor',
+  guest = 'guest',
 }
 
-// Замініть наступний код на версію за допомогою Record
-type UserDescription = Record<UserRole, string>;
-
-const RoleDescription: UserDescription = {
-  admin: "Admin User",
-  editor: "Editor User",
-  guest: "Guest User",
+const RoleDescription: Record<UserRole, string> = {
+  [UserRole.admin]: 'Admin User',
+  [UserRole.editor]: 'Editor User',
+  [UserRole.guest]: 'Guest User',
 };
 
 console.log(RoleDescription);
